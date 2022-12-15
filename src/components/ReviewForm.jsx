022e5515e0349162c80d837805ee7eac41aebafd
lauromiottojr <junior.miotto36@gmail.com>
2022-12-15 14:49:21 -0300

    ReviewForm component

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -1,6 +1,37 @@
+import { BsFillEmojiHeartEyesFill, BsFillEmojiSmileFill, BsFillEmojiNeutralFill, BsFillEmojiFrownFill } from 'react-icons/bs'
+
+import './ReviewForm.css'
+
 const ReviewForm = () => {
     return (
-        <div>ReviewForm</div>
+        <div className="reviewForm">
+            <div className="formControl scoreContainer">
+                <label className="radioContainer">
+                    <input type="radio" value="unsatisfied" name="review" required />
+                    <BsFillEmojiFrownFill />
+                    <p>Insatisfeito</p>
+                </label>
+                <label className="radioContainer">
+                    <input type="radio" value="neutral" name="review" required />
+                    <BsFillEmojiNeutralFill />
+                    <p>Poderia ser melhor</p>
+                </label>
+                <label className="radioContainer">
+                    <input type="radio" value="satisfied" name="review" required />
+                    <BsFillEmojiSmileFill />
+                    <p>Satisfeito</p>
+                </label>
+                <label className="radioContainer">
+                    <input type="radio" value="very-satisfied" name="review" required />
+                    <BsFillEmojiHeartEyesFill />
+                    <p>Muito satisfeito</p>
+                </label>
+            </div>
+            <div className="formControl">
+                <label htmlFor="comment">Comentário:</label>
+                <textarea name="comment" id="comment" placeholder='Conte como foi sua experiência' required></textarea>
+            </div>
+        </div>
     )
 }
 
