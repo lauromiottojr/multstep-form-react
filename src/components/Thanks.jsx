cb2d92b9d574a715d85d35d1f1447753bf7e8a46
lauromiottojr <junior.miotto36@gmail.com>
2022-12-15 15:59:58 -0300

    Config Thanks component

--- src/components/Thanks.jsx
+++ src/components/Thanks.jsx
@@ -1,6 +1,20 @@
+import { BsFillEmojiHeartEyesFill, BsFillEmojiSmileFill, BsFillEmojiNeutralFill, BsFillEmojiFrownFill } from 'react-icons/bs'
+
+import './Thanks.css'
+
 const Thanks = () => {
     return (
-        <div>Thanks</div>
+        <div className='thanksContainer'>
+            <p>A sua opnião é muito importante!</p>
+            <p>Para concluir a avaliação, clique no botão enviar agora.</p>
+            <h3>Aqui está o resumo da sua avaliação:</h3>
+            <p className="reviewData">
+                <span>Satisfação:</span>
+            </p>
+            <p className="reviewData">
+                <span>Comentário:</span>
+            </p>
+        </div>
     )
 }
 
