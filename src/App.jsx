0910e3850380aa14ce7d0b12e10d7d92ed29aa49
lauromiottojr <junior.miotto36@gmail.com>
2022-12-15 13:18:09 -0300

    Config hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,47 @@
+import { GrFormNext, GrFormPrevious } from 'react-icons/gr'
+
+import UserForm from './components/UserForm'
+import ReviewForm from './components/ReviewForm'
+import Thanks from './components/Thanks'
+
+import { useForm } from './hooks/useForm'
+
 import './App.css'
 
 function App() {
+
+  const formComponents = [
+    <UserForm />,
+    <ReviewForm />,
+    <Thanks />
+  ];
+
+  const { currentStep, currentComponent } = useForm(formComponents)
+
   return (
-    <div className="App">
-      <h1>react multstep</h1>
+    <div className="app">
+      <div className="header">
+        <h2>Deixe sua avaliação</h2>
+        <p>Ficamos felizes com sua compra, avalie sua experiência abaixo</p>
+      </div>
+      <div className="container">
+        <p>etapas</p>
+        <form action="">
+          <div className="inputsContainer">
+            {currentComponent}
+          </div>
+          <div className="actions">
+            <button type='button'>
+              <GrFormPrevious />
+              <span>Voltar</span>
+            </button>
+            <button type='submit'>
+              <span>Avançar</span>
+              <GrFormNext />
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
